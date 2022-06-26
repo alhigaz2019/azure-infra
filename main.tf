56001178c3e666117897a87437d8dd1586cf7f16
alhigaz2019 <alhigaz2019@gmail.com>
2022-06-25 20:30:38 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,43 @@
+terraform {
+  cloud {
+    organization "alhigaz2019gmail2"
+  
+    workspaces {
+      name = "azure-infra"
+    }
+  }
+}
+
+provider "azurem"
+  featurs {}
+
+  subscription_id = var.subscriptionID
+  client_id       = var.clientID
+  client_secret   = var.clientSecret
+  tenant_id       = var.tenantID
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example"
+  location = "East US 2"
+}
 resource "azurerm_resource_group" "devops" {
   name     = "devops"
   location = "East US 2"
 }
 
-resource "azurerm_resource_group" "webapp" {
-  name     = "webapp"
-  location = "East US 2"
+variable "subscriptionID" {
+  type = string
 }
+
+ variable "clientID" {
+  type = string
+}
+
+ variable "clientSecret" {
+  type = string
+ }
+
+ variable "tenantID" {
+  type = string
+ }
